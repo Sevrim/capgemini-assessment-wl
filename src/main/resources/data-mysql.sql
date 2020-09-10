ed5c52179d5f26346d9658ff502b0b060e7e8f9e
SebastianPeterFelixDewald <36820026+SebastianPeterFelixDewald@users.noreply.github.com>
2020-09-10 11:25:05 +0200

    first commit

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,20 @@
+INSERT INTO worker (id,name,branch) VALUES
+(1, 'Manuela' , 'Hannover'),
+(2, 'Karina', 'Hamburg'),
+(3, 'Jana', 'Duesseldorf'),
+(4, 'Sevcan', 'Duesseldorf'),
+(5, 'Adriana', 'Hamburg'),
+(6, 'Laura', 'Berlin'),
+(7, 'Lisa', 'Frankfurt'),
+(8, 'Indu', 'Muenchen'),
+(9, 'Iiulia', 'Frankfurt'),
+(10, 'Rainer', 'Duesseldorf'),
+(11, 'Ralf', 'Duesseldorf'),
+(12, 'Ray', 'Hamburg'),
+(13, 'Maik', 'Hannover'),
+(14, 'Mostafa', 'Hamburg'),
+(15, 'Moritz', 'Koeln'),
+(16, 'Andreas', 'Stuttgart'),
+(17, 'Sebastian', 'Hamburg')
+;
+
